7db27ca429e6a20f7a4e35490bb0803489e5c52d
Mariquena <allosamariquena@gmail.com>
2023-06-08 19:52:59 -0300

    Ejercicio 1

---
+++ src/guiaPractica2/ejercicio1.java
@@ -0,0 +1,17 @@
+package guiaPractica2;
+
+import java.util.Scanner;
+
+/**
+ *
+ * Escribir un programa que reciba un número entero por teclado. A
+ * continuación, mostrar la tabla de multiplicar de ese número.
+ *
+ */
+public class ejercicio1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Ingrese un número entero para generar la tabla de multipliacion");
+    }
+}
