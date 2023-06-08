4b72586d57b7e83b8f7aa96832b1400627bbc976
Mariquena <allosamariquena@gmail.com>
2023-06-08 19:18:38 -0300

    Create ejercicio6.java

---
+++ src/guiaPractica1/ejercicio6.java
@@ -0,0 +1,25 @@
+package guiaPractica1;
+
+import org.w3c.dom.ls.LSOutput;
+
+import java.util.Scanner;
+
+/**
+ * Pedir al usuario que ingrese el precio de un producto y el porcentaje
+ * de  descuento.  A  continuación  mostrar  por  pantalla  el  importe
+ * descontado y el importe a pagar.
+ */
+public class ejercicio6 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Ingrese el precio del producto");
+        float precio = scanner.nextFloat();
+
+        System.out.println("Ingrese el porcentaje de descuento");
+        float porcentaje = scanner.nextFloat();
+
+        float descuento = (porcentaje/100)* precio;
+        float importe = precio - descuento;
+
+        System.out.println("El importe descontado es " + descuento + " y el importe a pagar es "+ importe);
+    }}
