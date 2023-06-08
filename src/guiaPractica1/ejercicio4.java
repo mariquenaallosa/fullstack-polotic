febe3cccd94d09492731c7dc08bf6bab4c0cde0a
Mariquena <allosamariquena@gmail.com>
2023-06-08 18:56:32 -0300

    Create ejercicio4.java

---
+++ src/guiaPractica1/ejercicio4.java
@@ -0,0 +1,28 @@
+package guiaPractica1;
+
+import java.util.Scanner;
+
+/*
+*
+* Escribir un programa que lea la estatura de tres personas, calcule el promedio de la altura de ellos y lo informe
+*
+*/
+public class ejercicio4 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Ingrese la estatura de la primera persona");
+        double estatura1 = scanner.nextDouble();
+
+        System.out.println("Ingrese la estatura de la segunda persona");
+        double estatura2 = scanner.nextDouble();
+
+        System.out.println("Ingrese la estatura de la tercera persona");
+        double estatura3 = scanner.nextDouble();
+
+
+        double promedioAlturas = (estatura1 + estatura2 + estatura3)/3;
+
+        System.out.println("El promedio de las alturas ingresadas es: "+ promedioAlturas);
+
+    }
+}
