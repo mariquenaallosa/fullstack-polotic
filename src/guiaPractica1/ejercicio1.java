2e1a44be3fd4f45e97db479e49d90bcbebfe5932
Mariquena <allosamariquena@gmail.com>
2023-06-08 18:39:43 -0300

    Ejercicio 1

---
+++ src/guiaPractica1/ejercicio1.java
@@ -0,0 +1,13 @@
+package guiaPractica1;
+/*
+*
+* Hacer un programa que muestre por pantalla el texto "Hola Mundo"
+*
+*
+*/
+public class ejercicio1 {
+    public static void main(String[] args) {
+        System.out.println("Hola mundo");
+    }
+
+}
