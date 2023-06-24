61c08d2054e3234138ed557e8d9f5a15fd7adb72
Mariquena <allosamariquena@gmail.com>
2023-06-23 22:03:23 -0300

    .

--- src/guiaPractica2/Ejercicio3.java
+++ src/guiaPractica2/Ejercicio3.java
@@ -1,7 +1,6 @@
 package guiaPractica2;
 /**
- * Escribir un programa que ordene un arreglo de números enteros de
- * manera ascendente.
+ * 
  */
 public class Ejercicio3 {
     public static void main(String[] args) {
