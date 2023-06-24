74ee361fa188754a0d7701e59e2c08400b3c9fc5
Mariquena <allosamariquena@gmail.com>
2023-06-23 22:15:41 -0300

    Ordenar array de números

--- src/guiaPractica2/Ejercicio3.java
+++ src/guiaPractica2/Ejercicio3.java
@@ -1,10 +1,32 @@
 package guiaPractica2;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
 /**
- * 
+ * Escribir un programa que ordene un arreglo de números enteros de
+ * manera ascendente.
  */
 public class Ejercicio3 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Ingrese la cantidad de números enteros que ingresará: ");
+        int cantidad = scanner.nextInt();
+        int[] array = new int[cantidad];
 
+        for (int i = 0; i < cantidad; i++) {
+            System.out.print("Ingrese el " + (i + 1) + "º entero: ");
+            int numero = scanner.nextInt();
+            array[i] = numero;
+        }
+        scanner.close();
+
+        Arrays.sort(array);
+
+        System.out.println("Números ordenados:");
+        for (int i = 0; i < cantidad; i++) {
+            System.out.print(array[i] + "  ");
+        }
 
     }
 }
