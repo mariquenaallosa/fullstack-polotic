24100be6039681dd9d67cbf1993e73fbfa843cdb
Mariquena <allosamariquena@gmail.com>
2023-07-14 02:45:44 -0300

    Update Ejercicio5.java

--- src/guiaPractica2/Ejercicio5.java
+++ src/guiaPractica2/Ejercicio5.java
@@ -1,2 +1,31 @@
-package guiaPractica2;public class Ejercicio5 {
+package guiaPractica2;
+
+import java.util.Scanner;
+
+/**
+ * Escribir un algoritmo que lea un valor n por teclado y muestre los primeros n términos de la sucesión de Fibonacci.
+ */
+
+public class Ejercicio5 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Ingrese el número para generara la serie de Fibonacci correspondiente: ");
+        int cantidad = scanner.nextInt();
+        int[] fibonacci = new int[cantidad];
+
+        fibonacci[0]= 0;
+        fibonacci[1]= 1;
+
+        for (int i = 2; i < cantidad; i++){
+
+        }
+
+        scanner.close();
+
+        System.out.println("Números ordenados:");
+        for (int i = 0; i < cantidad; i++) {
+            System.out.print(fibonacci[i] + "  ");
+        }
+    }
+
 }
